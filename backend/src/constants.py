514cb069b70b029039d22d3b7d133c801f077278
PT_Chen <ptchen314@gmail.com>
2024-10-30 03:57:45 +0800

    Refactor: Add constants.py

---
+++ backend/src/constants.py
@@ -0,0 +1,7 @@
+from enum import Enum
+
+class ErrorCode:
+    INVALID_CREDENTIALS = "Invalid credentials"
+    USER_NOT_FOUND = "User not found"
+    NEWS_NOT_FOUND = "News not found"
+    INVALID_TOKEN = "Invalid token" 
